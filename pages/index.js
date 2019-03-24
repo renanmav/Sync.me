42ebfb1ddaf5ad11f82a7c9a7772525974676eb4
Renan Machado <renan.mav@hotmail.com>
2019-03-24 02:00:15 -0300

    withLayout HOC

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Header from '../components/Header';
+import withLayout from '../lib/withLayout';
 
 const Index = () => (
   <div style={{ padding: '10px 45px' }}>
@@ -7,9 +7,8 @@ const Index = () => (
       <title>Index page</title>
       <meta name="description" content="This is the description of the Index page" />
     </Head>
-    <Header />
     <p>Content on Index page</p>
   </div>
 );
 
-export default Index;
+export default withLayout(Index);
